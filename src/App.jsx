66e1476c2250884332dbb92ad8edddd7cd5308ef
Douglas Lima <douglas07a@gmail.com>
2024-02-19 12:22:40 -0300

    Criando estilo do NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <div className='App'>
       
-    <nav id='navbar'>
-
-      <h2>
-        <Link to="/">Movies</Link>
-      </h2>
-        <Link to="/Movie/1">Movie Filmes</Link>
-        <Link to="/search">Search</Link>
-    </nav>
-
-    <h2>Movies Lib</h2>
+    <NavBar/>
     <Outlet/>
 
     </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav id='navbar'>
+
+      <h2>
+        <Link to="/"><BiCameraMovie/>Movie</Link>
+      </h2>
+        <form>
+            <input type="text" placeholder="Busque um filme" />
+            <button type="submit">
+                <BiSearchAlt2/>
+            </button>
+        </form>
+    </nav>
+    )
+}
+
+export default NavBar;
